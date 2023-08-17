519fea8dcd2854ad825213bd6fd620ad04eba3e2
AlexQwerty73 <124087147+AlexQwerty73@users.noreply.github.com>
2023-08-17 17:14:57 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,39 @@
-const arr = [-2, 0, 3, 6, -24];
-const arr1 = [
-  {a: 1, b: 3},
-  {a: 11, b: 34}
-];
-Array.prototype.myForeach = myForeach
 
-arr.myForeach((item) => {
-  console.log(item);
-});
+Array.prototype.myForeach = myForeach
+Array.prototype.myMap = myMap
+Array.prototype.myFilter = myFilter
+Array.prototype.myFind = myFind
+
 
 function myForeach(f) {
   for (let i = 0; i < this.length; i++) {
-      f(this[i]);
+    f(this[i], i);
   }
+}
+function myMap(f) {
+  const newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    newArr.push(f(this[i]));
+  }
+  return newArr;
+}
+function myFilter(f) {
+  const filterArr = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (f(this[i], i)) {
+      filterArr.push(this[i]);
+    }
+  }
+
+  return filterArr;
+}
+function myFind(f) {
+  for (let i = 0; i < this.length; i++) {
+    if (f(this[i], i)) {
+      return this[i];
+    }
+  }
+
+  return undefined;
 }
