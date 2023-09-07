518fb644d1a72b92ce99ae9ba6870bca736ffb6a
AlexQwerty73 <124087147+AlexQwerty73@users.noreply.github.com>
2023-09-07 20:10:07 +0200

    lesson

--- js/API/api.js
+++ js/API/api.js
@@ -1,10 +1,18 @@
-export const baseUrl = 'https://swapi.dev/api/';
-const header = {
-    'Content-Type': 'application/json'
-}
+const baseUrl = 'https://swapi.dev/api/';
+const headers = {
+  'Content-type': 'Application/json',
+};
 
 export const getResources = async () => {
-    const res = await fetch(baseUrl, {header});
-    const resources = await res.json();
-    return resources;
+  const res = await fetch(baseUrl, { headers });
+  const resources = await res.json();
+
+  return resources;
+}
+
+export const getData = async (resource = '') => {
+  const res = await fetch(baseUrl + resource, { headers });;
+  const data = await res.json();
+
+  return data;
 }
--- js/main.js
+++ js/main.js
@@ -1,38 +1,4 @@
-const doc = document;
-import { getResources } from "./API/api.js";
-import { baseUrl } from "./API/api.js";
+import { browserRoute } from "./routes/browserRouter.js";
 
-let resources = null;
+browserRoute();
 
-renderResource()
-
-async function renderResource() {
-    const resourcesHtmlEl = doc.querySelector('.resources');
-    resources = !resources ? await getResources() : resources;
-
-    const resourcesEls =
-        Object.keys(resources)
-            .map((resource) => {
-                return `<li class="resource btn">
-                    <a href="#/${resource}/">
-                        ${resource.charAt(0).toUpperCase() + resource.slice(1)}
-                    </a>
-                </li>`})
-            .join('');
-
-    resourcesHtmlEl.innerHTML = resourcesEls;
-
-    addEventListener('hashchange', () => {
-        const resourceLinks = resourcesHtmlEl.querySelectorAll('.resources li a');
-        resourceLinks.forEach((link) => {
-            link.classList.remove('active');
-        });
-        //.classList.add('active');
-
-        //    const url = baseUrl + hash.slice(1);
-        //    console.log(url);
-
-    });
-
-    console.log(resources);
-}
