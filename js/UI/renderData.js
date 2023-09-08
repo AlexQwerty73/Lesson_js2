51134752341ad98ff9d1f1f6cf8dca33ea2d931d
AlexQwerty73 <124087147+AlexQwerty73@users.noreply.github.com>
2023-09-08 22:59:34 +0200

    hw

--- js/UI/renderData.js
+++ js/UI/renderData.js
@@ -1,40 +1,51 @@
 import { getData } from "../API/api.js";
-import { getHash } from "../utils/utils.js"
+import { getHash } from "../utils/utils.js";
 
 const doc = document;
 
-let data = null;
+const cachedData = {};
 
 export const renderData = async () => {
-  const hash = getHash().replace('/', '');
-  const parEl = doc.querySelector('.data')
+  const hash = getHash();
+  const parEl = doc.querySelector('.data');
 
-  const lastIndex = location.href.lastIndexOf('/');
-  const idElement = Number(location.href.slice(lastIndex + 1));
-  let html;
+  //бере усе після останього /
+  const idElement = Number(location.href.slice(location.href.lastIndexOf('/') + 1));
 
   if (!hash) return;
 
   if (Number.isInteger(idElement)) {
-    data = await getData(hash.slice(0, lastIndex) + hash.slice(lastIndex + 1));
-    html = `<div class="btn data-item">${Object.keys(data).map(key => `<li>${key}: ${data[key]}</li>`)}</div>`;
+    // прибирає усе після останього /
+    const nh = hash.substring(0, hash.lastIndexOf('/'));
+
+    //якщо нема в cachedData то заносить туди данні 
+    if (!cachedData[nh]) {
+      cachedData[nh] = await getData(nh);
+    }
+
+    const data = cachedData[nh];
+
+    const html = `<div class="btn data-item">
+    ${Object.keys(data['results'][idElement])
+        .map(key => `<li>${key}: ${data['results'][idElement][key]}</li>`).join('')}</div>`;
 
     parEl.innerHTML = html;
     return;
-
-  } else {
-    data = await getData(hash);
   }
 
-  console.log(data);
-  html =
+  //якщо нема в cachedData то заносить туди данні 
+  if (!cachedData[hash]) {
+    cachedData[hash] = await getData(hash);
+  }
+
+  const data = cachedData[hash];
+
+  const html =
     Object.keys(data['results'])
       .map((key, i) => `<div class="btn data-item"><a href="#/${hash}/${i}">${Object.keys(data['results'][key])
         .map(k => `<li>${k}: ${data['results'][key][k]}</li>`)
         .join('')}</a></div>`)
       .join('');
 
-
   parEl.innerHTML = html;
-
-}
+};
